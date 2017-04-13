f796d39f01ee4d3e07bc24efdf5f93712849e159
Marco <untalmarco8@gmail.com>
2017-04-13 01:35:17 -0600

    App docente- Ver clases y sus alumnos

--- docente/urls.py
+++ docente/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^veralumnos/$', views.veralumnos, name='veralumnos'),
-	url(r'^vermisclases/$', views.vermisclases, name='vermisclases'),
-	url(r'^ingresarnota/$', views.ingresarnota, name='ingresarnota'),
+	url(r'^$', views.index_docente, name='index_docente'),
+	url(r'^ver_alumnos/(?P<pk>\d+)/$', views.ver_alumnos, name='ver_alumnos'),
+	url(r'^ver_misclases/$', views.ver_misclases, name='ver_misclases'),
+	url(r'^ingresarnota/(?P<pk>\d+)/$', views.ingresarnota, name='ingresarnota'),
 
 ]
--- docente/views.py
+++ docente/views.py
@@ -1,17 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from admini.models import *
 
 from django.contrib.auth.models import User
 # Create your views here.
 
 @login_required()
-def veralumnos(request):
-	return render(request, 'veralumnos.html')
+def index_docente(request):
+	if request.user.Perfil.es_docente:
+		return render(request, 'index_docente.html')
+	else:
+		return render(request, 'denegado.html')
+
+
 
 @login_required()
-def ingresarnota(request):
-	return render(request, 'ingresarnotas.html')
+def ingresarnota(request, pk):
+	detm = []
+	if request.user.Perfil.es_docente:
+		of = OfertaClase.objects.get(pk=pk)
+		mat = Matricula.objects.filter(ofertagrado=of.ofertagrado)
+		for matri in mat:
+			detm2 = DetalleMatricula.objects.filter(matricula=matri)
+			detm.append(detm2)
+			
+		return render(request, 'ingresarnotas.html', {'detm': detm, 'of': of})
+		#return HttpResponse(detm2)
+	else:
+		return render(request, 'denegado.html')
+
 
 @login_required()
-def vermisclases(request):
-	return render(request, 'vermisclases.html')
+def ver_misclases(request):
+	if request.user.Perfil.es_docente:
+		misclases = OfertaClase.objects.filter(docente=request.user)
+		return render(request, 'ver_misclases.html', {'misclases': misclases})
+	else:
+		return render(request, 'denegado.html')
+
+@login_required()
+def ver_alumnos(request, pk):
+	if request.user.Perfil.es_docente:
+		of = OfertaClase.objects.get(pk=pk)
+		mat = Matricula.objects.filter(ofertagrado=of.ofertagrado)
+
+
+		return render(request, 'ver_alumnos.html', {'mat': mat, 'of': of})
+	else:
+		return render(request, 'denegado.html')
